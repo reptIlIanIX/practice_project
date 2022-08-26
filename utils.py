2fd9e5eacb296510e651d1497bd93ab53a783ec2
Denis_b <spawnslash@mail.ru>
2022-08-26 14:38:03 +0300

    commiting

--- utils.py
+++ utils.py
@@ -6,13 +6,18 @@ app = Flask(__name__)
 with open('candidates.json') as f:
     data = json.load(f)
 
-items = []
-
 
 def load_candidates_from_json(path):
     for item in data:
-        path.append([item['name']])
+        path.append(item['name'])
     return path
 
 
-print(load_candidates_from_json(items))
+@app.route('/')
+def hello():
+    items = []
+    items = load_candidates_from_json(items)
+    return render_template('list.html', items=items)
+
+
+app.run()
