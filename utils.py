23ed99e194b99816820962dd275d2b413c5d8790
reptIlIanIX <reptilianix@gmail.com>
2022-09-15 17:05:20 +0300

    done

--- utils.py
+++ utils.py
@@ -3,29 +3,63 @@ from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-with open('candidates.json') as f:
-    data = json.load(f)
+
+def load_candidates_from_json():
+    with open('candidates.json', 'r', encoding="utf-8") as file:
+        return json.load(file)
 
 
-def load_candidates_from_json(path):
-    for item in data:
-        path.append(item['name'])
-    return path
+def get_candidate(candidate_id: int):
+    for candidate in load_candidates_from_json():
+        if candidate['id'] == candidate_id:
+            return candidate
 
 
-def get_candidate(candidate_id):
-    for item in data:
-        if candidate_id == item['id']:
-            candidate_id += 1
-    return item['name']
+def get_candidates_by_name(candidate_name: str):
+    result = []
+    for candidate in load_candidates_from_json():
+        if candidate['name'] == candidate_name:
+            result.append(candidate)
+            return result
+
+
+def get_candidates_by_skill(skill_name: str):
+    result = []
+    for candidate in load_candidates_from_json():
+        if skill_name in candidate["skills"].lower():
+            result.append(candidate)
+    return result
+
+
+print(get_candidates_by_skill('python'))
 
 
 @app.route('/')
 def hello():
-    items = []
-    items = load_candidates_from_json(items)
+    candidates = load_candidates_from_json()
+    return render_template('list.html', candidates=candidates)
 
-    return render_template('list.html', items=items)
+
+@app.route('/candidate/<int:idx>')
+def candidate_list(idx: int):
+    candidate = get_candidate(idx)
+    if not candidate:
+        return "Кандидат не найден"
+    return render_template('single.html', candidate=candidate)
+
+
+@app.route('/search/<candidate_names>')
+def candidate_name(candidate_names):
+    candidates = get_candidates_by_name(candidate_names)
+    count = len(candidates)
+    return render_template('search.html', candidates=candidates, count=count)
+
+
+@app.route('/skill/<skill_name>')
+def candidate_skill(skill_name):
+    candidates = get_candidates_by_skill(skill_name)
+    count = len(candidates)
+    return render_template('skill.html', candidates=candidates, count=count)
 
 
 app.run()
